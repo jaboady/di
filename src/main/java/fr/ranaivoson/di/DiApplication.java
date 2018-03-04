b3db33c23b30ad59b4cf476f2c1bc4f98a4dc4c3
mr <ranaivoson@orange.fr>
2018-03-04 21:16:16 +0100

    Initial commit

---
+++ src/main/java/fr/ranaivoson/di/DiApplication.java
@@ -0,0 +1,19 @@
+package fr.ranaivoson.di;
+
+import fr.ranaivoson.di.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
+	}
+}
